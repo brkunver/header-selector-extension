eab6edc7cde5a01a158acbaffd0e13f532ad2952
burakhanunver <burakhanunver@gmail.com>
2023-08-06 20:37:55 +0300

    v1

--- popup.tsx
+++ popup.tsx
@@ -1,6 +1,7 @@
 import "./style.css"
 
 import { useState } from "react"
+import { v4 as uuidv4 } from "uuid"
 
 function IndexPopup() {
   const [showHeaderSection, setShowHeaderSection] = useState(false)
@@ -26,35 +27,51 @@ function IndexPopup() {
     setShowHeaderSection(true)
   }
 
+  function handleCopy() {
+    let headerList: string[] = []
+    for (let title of Object.values(headers)) {
+      if (title.length > 1) {
+        title.forEach((item) => {
+          if (item) headerList.push(item)
+        })
+      }
+    }
+    console.log(JSON.stringify(headerList))
+    navigator.clipboard.writeText(JSON.stringify(headerList))
+  }
+
   return (
-    <div className="w-[480] p-2 h-[200] overflow-y-auto bg-gradient-to-b from-emerald-900 to-cyan-900 text-white">
-      <h1 className="mt-6">Click button the get headers on this page</h1>
-      <button
-        onClick={getHeadersHandler}
-        className="mx-auto my-4 block border border-gray-600 font-semibold bg-emerald-800 px-2 rounded-lg py-1 text-base hover:scale-105 transition-all ease-in-out duration-100 hover:bg-emerald-700 active:bg-emerald-600">
+    <div className="w-[480] p-2 h-[300] overflow-y-auto bg-gradient-to-b from-emerald-900 to-cyan-900 text-white">
+      <h1 className="mt-6">Click button to get headers on this page</h1>
+      <button onClick={getHeadersHandler} className="">
         Get headers
       </button>
       {showHeaderSection ? (
-        <div className="flex flex-col justify-center my-4 ">
+        <button onClick={handleCopy}>
+          Copy all headers in JSON array text format
+        </button>
+      ) : null}
+      {showHeaderSection ? (
+        <div className="flex flex-col justify-center my-4 space-y-3">
           <div>
             <h2>H1 headers</h2>
 
             {headers.header1.map((item) => {
-              return <p>➔ {item}</p>
+              return <p key={uuidv4()}>➔ {item}</p>
             })}
           </div>
           <div>
             <h2>H2 headers</h2>
 
             {headers.header2.map((item) => {
-              return <p>➔ {item}</p>
+              return <p key={uuidv4()}>➔ {item}</p>
             })}
           </div>
           <div>
             <h2>H3 headers</h2>
 
             {headers.header3.map((item) => {
-              return <p>➔ {item}</p>
+              return <p key={uuidv4()}>➔ {item}</p>
             })}
           </div>
 
@@ -62,21 +79,21 @@ function IndexPopup() {
             <h2>H4 headers</h2>
 
             {headers.header4.map((item) => {
-              return <p>➔ {item}</p>
+              return <p key={uuidv4()}>➔ {item}</p>
             })}
           </div>
           <div>
             <h2>H5 headers</h2>
 
             {headers.header5.map((item) => {
-              return <p>➔ {item}</p>
+              return <p key={uuidv4()}>➔ {item}</p>
             })}
           </div>
           <div>
             <h2>H6 headers</h2>
 
             {headers.header6.map((item) => {
-              return <p>➔ {item}</p>
+              return <p key={uuidv4()}>➔ {item}</p>
             })}
           </div>
         </div>
